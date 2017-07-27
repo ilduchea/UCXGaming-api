8ddb0b4a8ab80eff3b954ed8dd52510ce43791b1
ilduchea <ilduchea@gmail.com>
2017-07-26 22:36:00 -0700

    add routes. remove redundent mongoid.yml

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,23 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
-  resources :game_systems
+  concern :sectional do
+    resources :sections
+  end
+
+  resources :game_systems do
+    concerns :sectional
+    resources :character_sheets
+  end
+
+  resources :character_sheets do
+    concerns :sectional
+  end
+
+  resources :characters do
+    concerns :sectional
+  end
+
+  resources :sections do
+    resources :traits
+  end
 end
